bbacef2004514e274acbfe4193c48a043d746231
Michael Hudson <michaelhudson2205@gmail.com>
2024-04-22 21:16:19 +1000

    Part way through doubly linked lists.

---
+++ src/ds/doublylinkedlist/DoublyLinkedList.java
@@ -0,0 +1,122 @@
+package ds.doublylinkedlist;
+
+public class DoublyLinkedList
+{
+	private Node first;
+	private Node last;
+	
+	public DoublyLinkedList()
+	{
+		this.first = null;
+		this.last = null;
+	}
+	
+	public boolean isEmpty()
+	{
+		return first == null;
+	}
+	
+	public void insertFirst(int data)
+	{
+		Node newNode = new Node();
+		newNode.data = data;
+		
+		if (isEmpty())
+		{
+			last = newNode; // if empty, last will refer to the new Node being created
+		}
+		else
+		{
+			first.previous = newNode;
+			newNode.next = first;
+		}
+		
+		this.first = newNode;
+	}
+	
+	public void insertLast(int data)
+	{
+		Node newNode = new Node();
+		newNode.data = data;
+		
+		if (isEmpty())
+		{
+			first = newNode;
+		}
+		else
+		{
+			last.next = newNode;
+			newNode.previous = last;
+		}
+		
+		this.last = newNode;
+	}
+	
+	// assume non-empty list
+	public Node deleteFirst()
+	{
+		Node temp = first;
+		if (first.next == null)
+		{
+			last = null;
+		}
+		else
+		{
+			first.next.previous = null;
+		}
+		
+		first = first.next;
+		return temp;
+	}
+	
+	// assume non-empty list
+	public Node deleteLast()
+	{
+		Node temp = last;
+		if (last.previous == null)
+		{
+			first = null;
+		}
+		else
+		{
+			last.previous.next = null;
+		}
+		
+		last = last.previous;
+		return temp;
+	}
+	
+	// assume non-empty list
+	public boolean insertAfter(int key, int data)
+	{
+		Node current = first;
+		while(current.data != key)
+		{
+			current = current.next;
+			if (current == null)
+			{
+				return false;
+			}
+		}
+		
+		Node newNode = new Node();
+		newNode.data = data;
+		
+		if (current == last)
+		{
+			current.next = null;
+			last = newNode;
+		}
+		else
+		{
+			newNode.next = current.next;
+			current.next.previous = newNode;
+		}
+		
+		newNode.previous = current;
+		current.next = newNode;
+		
+		return true;
+		
+	} // ==========end of insertAfter==========
+} // ==========end of class DoublyLinkedList==========
